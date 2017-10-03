0cdef6cb5673707ee24ab73286abc04aa526c99c
EroX <mamwasnahita@gmail.com>
2017-10-03 12:17:18 +0200

    Added method for getting filters as list

--- FiltersChecking/src/models/FiltersListModel.java
+++ FiltersChecking/src/models/FiltersListModel.java
@@ -1,7 +1,14 @@
 package models;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.DefaultListModel;
 
 public class FiltersListModel extends DefaultListModel<Filter> {
 	private static final long serialVersionUID = 5686217210155210320L;
+
+	public List<Filter> getFiltersAsList() {
+		return Arrays.asList((Filter[])this.toArray());	
+	}
 }
