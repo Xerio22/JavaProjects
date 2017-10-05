d1b100c986882d2ad66609ccfa46f7474fbcb7ae
EroX <mamwasnahita@gmail.com>
2017-10-05 12:15:42 +0200

    Added Filter's ListModel Reader Class

---
+++ FiltersChecking/src/main/FiltersReaderFromListModel.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ListModel;
+
+import models.Filter;
+
+public class FiltersReaderFromListModel implements FiltersReader {
+	private ListModel<Filter> filtersListModel;
+	
+	public FiltersReaderFromListModel(ListModel<Filter> filtersListModel) {
+		this.filtersListModel = filtersListModel;
+	}
+	
+	@Override
+	public List<Filter> getFiltersAsList() {
+		List<Filter> filters = new ArrayList<>();
+		
+		for(int i = 0; i < filtersListModel.getSize(); i++){
+			filters.add(filtersListModel.getElementAt(i));
+		}
+		return filters;
+	}
+
+}
