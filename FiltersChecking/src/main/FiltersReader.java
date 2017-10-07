6835b767be4593a7648b157bc7c174a3b6649da4
EroX <mamwasnahita@gmail.com>
2017-10-07 22:40:42 +0200

    Removed unused exception throw

--- FiltersChecking/src/main/FiltersReader.java
+++ FiltersChecking/src/main/FiltersReader.java
@@ -5,5 +5,5 @@ import java.util.List;
 import models.Filter;
 
 public interface FiltersReader {
-	public List<Filter> getFiltersAsList() throws Exception;
+	public List<Filter> getFiltersAsList();
 }
