354ae35d4cdd516d568423547efbb2b66a780ba3
EroX <mamwasnahita@gmail.com>
2017-10-07 23:16:41 +0200

    Created filters checkers package and site dependent Filters Checkers

---
+++ FiltersChecking/src/filterscheckers/MannChecker.java
@@ -0,0 +1,15 @@
+package filterscheckers;
+
+import java.util.List;
+
+import models.Filter;
+
+public class MannChecker implements FiltersChecker {
+
+	@Override
+	public List<Filter> runFiltersChecking() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
