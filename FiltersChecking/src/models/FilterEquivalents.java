767c298c6dd023e9fd4534c32743fa296ca65286
EroX <mamwasnahita@gmail.com>
2017-10-08 23:07:48 +0200

    Added method to create equivalent directly from parsing results

--- FiltersChecking/src/models/FilterEquivalents.java
+++ FiltersChecking/src/models/FilterEquivalents.java
@@ -13,4 +13,25 @@ public class FilterEquivalents {
 	public List<Filter> getEquivalents() {
 		return equivalents;
 	}
+
+	public void createAndAddEquivalent(String searchResultOEM, String equvalentBrand, String equivalentNumber, int propIdx) {
+		Filter newEquivalent = createEquivalent(searchResultOEM, equvalentBrand, equivalentNumber, propIdx);
+		this.addEquivalent(newEquivalent);
+	}
+	
+	private Filter createEquivalent(String searchResultOEM, String equvalentBrand, String equivalentNumber, int propIdx){
+		// Create new properties with brand and number values and add them to list of replacement's properties
+		FilterProperty searchResultOEMnameProperty = new FilterProperty("equiv_" + propIdx + "_OEM", searchResultOEM);
+		FilterProperty equivalentBrandProperty = new FilterProperty("equiv_" + propIdx + "_brand", equvalentBrand);
+		FilterProperty equivalentNumberProperty = new FilterProperty("equiv_" + propIdx + "_replacement", equivalentNumber);
+		
+		List<FilterProperty> properties = new ArrayList<>();
+		properties.add(searchResultOEMnameProperty);
+		properties.add(equivalentBrandProperty);
+		properties.add(equivalentNumberProperty);
+		
+		Filter equivalent = Filter.createFilterFromProperties(properties);
+		
+		return equivalent;
+	}
 }
