d5414be38a07b5481a8996ffa92b48ee0cc9bbc2
EroX <mamwasnahita@gmail.com>
2017-10-13 23:36:09 +0200

    Created new handler type for handling JS based connections

---
+++ FiltersChecking/src/connectionhandlers/JSBasedConnectionHandler.java
@@ -0,0 +1,23 @@
+package connectionhandlers;
+
+public class JSBasedConnectionHandler extends ServerConnectionHandler {
+
+	@Override
+	public void insertFilterNameIntoUrlString(String searchedFilterOEMnumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getServerResponse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkIsAnyReplacementPresent(String serverResponse) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
