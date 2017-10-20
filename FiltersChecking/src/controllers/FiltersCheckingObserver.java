70d576a33da92d8eb1da47eeae72fef83e5f396d
EroX <mamwasnahita@gmail.com>
2017-10-20 17:10:55 +0200

    Created new Observer used for control list and tabs

---
+++ FiltersChecking/src/controllers/FiltersCheckingObserver.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import models.Filter;
+import views.CheckedFilterTab;
+
+public class FiltersCheckingObserver implements Observer {
+
+	private JTabbedPane tabsPanel;
+	private JList<Filter> filtersList;
+
+	public FiltersCheckingObserver(JList<Filter> filtersList, JTabbedPane tabsPanel) {
+		this.filtersList = filtersList;
+		this.tabsPanel = tabsPanel;
+	}
+
+	@Override
+	public void update(Observable observable, Object filter) {
+		// TODO if it would be need for having a message I can create special class CheckedFilter containing checked filter and attached messages 
+		addNewTabWithCheckedFilterData((Filter) filter);
+		removeCheckedFilterFromList();
+	}
+	
+	private void addNewTabWithCheckedFilterData(Filter filter) {
+		Filter checkedFilter = filter;
+		tabsPanel.addTab(checkedFilter.getOemNumber(), new CheckedFilterTab(checkedFilter));
+	}
+
+	
+	private void removeCheckedFilterFromList() {
+		DefaultListModel<Filter> listModel = (DefaultListModel<Filter>) filtersList.getModel();
+		listModel.removeElementAt(0);
+	}
+}
