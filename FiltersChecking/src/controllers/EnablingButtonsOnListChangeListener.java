bd5ebbd7f8b2cfdee92372667c50a2e8a834f12d
EroX <mamwasnahita@gmail.com>
2017-10-21 23:10:29 +0200

    Rebuilt, to implement new way of selecting list's items

--- FiltersChecking/src/controllers/EnablingButtonsOnListChangeListener.java
+++ FiltersChecking/src/controllers/EnablingButtonsOnListChangeListener.java
@@ -1,30 +1,54 @@
 package controllers;
 
+import java.util.List;
+
 import javax.swing.JButton;
-import javax.swing.ListModel;
+import javax.swing.JList;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
-public class EnablingButtonOnListChangeListener implements ListDataListener {
-	private ListModel<?> listModel;
-	private JButton button;
+public class EnablingButtonsOnListChangeListener implements ListDataListener {
+	private JList<?> jlist;
+	private List<JButton> buttons;
 
-	public EnablingButtonOnListChangeListener(ListModel<?>filtersListModel, JButton button) {
-		this.listModel = filtersListModel;
-		this.button = button;
+	public EnablingButtonsOnListChangeListener(JList<?> jlist, List<JButton> buttons) {
+		this.jlist = jlist;
+		this.buttons = buttons;
 	}
 	
 	@Override
 	public void intervalRemoved(ListDataEvent e) {
-		if(listModel.getSize() == 0){
-			button.setEnabled(false);
+		if(jlist.getModel().getSize() == 0){
+			setButtonsEnabled(false);
+		}
+		
+		int index = e.getIndex0();
+		int actualSize = jlist.getModel().getSize();
+		
+		if(index == 0 && actualSize != 1){
+			jlist.setSelectedIndex(index + 1);
+		}
+		else if(actualSize == 1){
+			jlist.setSelectedIndex(1);
+			jlist.setSelectedIndex(0);
+		}
+		else{
+			jlist.setSelectedIndex(index - 1);
 		}
 	}
 	
 	@Override
-	public void intervalAdded(ListDataEvent e) {button.setEnabled(true);}
+	public void intervalAdded(ListDataEvent e) {
+		jlist.setSelectedIndex(jlist.getModel().getSize() - 1);
+		setButtonsEnabled(true);
+	}
 	
 	
 	@Override
 	public void contentsChanged(ListDataEvent e) {}
+	
+	
+	private void setButtonsEnabled(boolean isEnabled){
+		buttons.forEach(button -> button.setEnabled(isEnabled));
+	}
 }
