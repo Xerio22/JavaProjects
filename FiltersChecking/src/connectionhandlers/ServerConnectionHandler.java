14258ff42cfe9583cb5e66bb235b49df7674bbf5
EroX <mamwasnahita@gmail.com>
2017-10-21 15:48:56 +0200

    Added new psf with URL error

--- FiltersChecking/src/connectionhandlers/ServerConnectionHandler.java
+++ FiltersChecking/src/connectionhandlers/ServerConnectionHandler.java
@@ -6,6 +6,7 @@ public abstract class ServerConnectionHandler extends Observable {
 	public static final String RECONNECT_MESSAGE = "Reconnect";
 	public static final String CONNECTING_MESSAGE = "Connecting";
 	public static final String CONNECTED_MESSAGE = "Connected";
+	public static final String URL_ERROR = "URL_error";
 	
 	public abstract void supplyFilterOEMnumber(String searchedFilterOEMnumber);
 	
