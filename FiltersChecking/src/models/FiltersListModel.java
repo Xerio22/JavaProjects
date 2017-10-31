867b94b5dcb169c22d461f53fad5491b70422a03
EroX <mamwasnahita@gmail.com>
2017-11-01 00:40:43 +0100

    Added method for adding whole liest to ListModel

--- FiltersChecking/src/models/FiltersListModel.java
+++ FiltersChecking/src/models/FiltersListModel.java
@@ -1,7 +1,15 @@
 package models;
 
+import java.util.List;
+
 import javax.swing.DefaultListModel;
 
 public class FiltersListModel extends DefaultListModel<Filter> {
 	private static final long serialVersionUID = 5686217210155210320L;
+
+	public void addAll(List<Filter> list) {
+		for(Filter filter : list) {
+			this.addElement(filter);
+		}
+	}
 }
