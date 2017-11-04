dbd990bc791fbf8d810fad12b3c7425b63325c2a
EroX <mamwasnahita@gmail.com>
2017-11-04 21:32:43 +0100

    Added field to share within subclasses, added superclass constructor

--- FiltersChecking/src/connectionhandlers/ServerConnectionHandler.java
+++ FiltersChecking/src/connectionhandlers/ServerConnectionHandler.java
@@ -7,8 +7,16 @@ public abstract class ServerConnectionHandler extends Observable {
 	public static final String CONNECTING_MESSAGE = "Connecting";
 	public static final String CONNECTED_MESSAGE = "Connected";
 	public static final String URL_ERROR = "URL_error";
+	private String serverUrlString;
+	
+	public ServerConnectionHandler(String serverUrlString) {
+		this.serverUrlString = serverUrlString;
+	}
 	
 	public abstract void supplyFilterOEMnumber(String searchedFilterOEMnumber);
-	
 	public abstract String getServerResponse();
+	
+	public String getServerUrlString(){
+		return serverUrlString;
+	}
 }
