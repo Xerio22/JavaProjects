b77012e0fb211da5c191ec3f763f5921eeb4bd0c
EroX <mamwasnahita@gmail.com>
2017-11-04 21:12:12 +0100

    Implemented Wix Checker

--- FiltersChecking/src/filterscheckers/WixChecker.java
+++ FiltersChecking/src/filterscheckers/WixChecker.java
@@ -1,13 +1,16 @@
 package filterscheckers;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import connectionhandlers.ServerConnectionHandler;
 import connectionhandlers.URLBasedConnectionHandler;
 import models.FilterEquivalents;
 
 public class WixChecker extends FilterChecker {
 	private static final String CHECKER_NAME = "Wix";
-	private static final String SERVER_URL_STRING = "_FILTERNAME_";
-	private static final String SUCCESS_RESPONSE = "some_success_response";
+	private static final String SERVER_URL_STRING = "http://www.wixfilters.com/Lookup/InterchangeMultiSearch.aspx?q=_FILTERNAME_&o=me";
+	private static final String SUCCESS_RESPONSE = "table class=";
 	private static final String BLOCKED_BY_SERVER_RESPONSE = "some_blocked_by_server_response";
 	private static final ServerConnectionHandler connectionHandler = new URLBasedConnectionHandler(SERVER_URL_STRING);
 	
@@ -17,7 +20,51 @@ public class WixChecker extends FilterChecker {
 
 	@Override
 	protected FilterEquivalents parseServerResponseAndGetEquivalents(String serverResponse) {
-		return null;
+		Pattern p = Pattern.compile(
+				"<span id=\"lblCompetitor\">(.*?)</span>\\s*" // oem number
+				+ "<span id=\"lblOE\">\\s*</span>\\s*"
+				+ "</font>\\s*"
+				+ "</td>\\s*"
+				+ "<td>\\s*"
+				+ "<font face=\"Verdana\" color=\"#333333\">"
+				+ "\\s*(.*?)\\s*" // oem
+				+ "</font>\\s*"
+				+ "</td>\\s*"
+				+ "<td>\\s*"
+				+ "<font face=\"Verdana\" color=\"#333333\">\\s*"
+				+ "<a id=\"hlPartNumber\" href=\"javascript:var .*?;w\\.focus\\(\\);\">(.*?)</a>" // wix number
+		);
+
+		Matcher m = p.matcher(serverResponse);
+		
+		String equivalentOEMNumber = null;
+		String equivalentOEM = null;
+		String equivalentNumber = null;
+		
+		FilterEquivalents equivalentsForThisOem = new FilterEquivalents();
+		
+		int propIdx = 1;
+		
+			
+			while(m.find()){
+				System.out.println("ferferg");
+				equivalentOEM = m.group(1);
+				equivalentOEMNumber = m.group(2);
+				equivalentNumber = m.group(3);
+
+				equivalentsForThisOem.createAndAddEquivalent(
+						getCheckerName(), 
+						equivalentOEMNumber, 
+						equivalentOEM, 
+						equivalentNumber, 
+						propIdx
+				);
+				
+				propIdx++;
+			}
+		
+		
+		return equivalentsForThisOem;
 	}
 
 	@Override
