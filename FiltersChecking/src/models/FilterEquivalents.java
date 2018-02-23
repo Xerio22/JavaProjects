9ce9e995e460a154fdcef94fea09328d531d0177
EroX <mamwasnahita@gmail.com>
2018-02-23 22:18:07 +0100

    Adjusted line breaks for better readability

--- FiltersChecking/src/models/FilterEquivalents.java
+++ FiltersChecking/src/models/FilterEquivalents.java
@@ -6,18 +6,22 @@ import java.util.List;
 public class FilterEquivalents {
 	private List<Filter> equivalents = new ArrayList<>();
 	
+	
 	public void addEquivalent(Filter equivalent) {
 		equivalents.add(equivalent);
 	}
 	
+	
 	public List<Filter> getEquivalents() {
 		return equivalents;
 	}
+	
 
 	public void createAndAddEquivalent(String checkerName, String equivalentOEM, String equvalentBrand, String equivalentNumber, int propIdx, String[]... additionalPropertiesWithNames) {
 		Filter newEquivalent = createEquivalent(checkerName, equivalentOEM, equvalentBrand, equivalentNumber, propIdx, additionalPropertiesWithNames);
 		this.addEquivalent(newEquivalent);
 	}
+	
 	
 	private Filter createEquivalent(String checkerName, String equivalentOEM, String equvalentBrand, String equivalentNumber, int propIdx, String[]... additionalPropertiesWithNames){
 		// Create new properties with brand and number values and add them to list of replacement's properties
@@ -38,10 +42,12 @@ public class FilterEquivalents {
 		
 		return equivalent;
 	}
+	
 
 	private String prepareFullPropertyNameForEquivalent(String checkerName, String propertyName, int propIdx){
 		return checkerName + "_equiv_" + propIdx + "_" + propertyName;
 	}
+	
 	
 	private List<FilterProperty> createAdditionalPropertiesFromNameValueArray(String[][] additionalPropertiesWithNames, String checkerName, int propIdx) {
 		List<FilterProperty> properties = new ArrayList<>();
@@ -51,7 +57,6 @@ public class FilterEquivalents {
 					prepareFullPropertyNameForEquivalent(checkerName, additionalPropertiesWithNames[i][0], propIdx), 
 					additionalPropertiesWithNames[i][1]
 			);
-			
 			properties.add(prop);
 		}
 		
