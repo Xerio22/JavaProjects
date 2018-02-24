bdf33cdaff808013e1dd335bd203498af15c91c9
EroX <mamwasnahita@gmail.com>
2018-02-24 14:03:43 +0100

    Implemented new abstraction for handling filters with leading zeros

--- FiltersChecking/src/filterscheckers/BaldwinChecker.java
+++ FiltersChecking/src/filterscheckers/BaldwinChecker.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 import connectionhandlers.JSBasedConnectionHandler;
 import connectionhandlers.ServerConnectionHandler;
+import models.Filter;
 import models.FilterEquivalents;
 
 public class BaldwinChecker extends FilterChecker {
@@ -12,7 +13,7 @@ public class BaldwinChecker extends FilterChecker {
 	private static final String SERVER_URL_STRING = "http://stage.catalog.baldwinfilter.com/Cross-Reference";
 	private static final String SUCCESS_RESPONSE = "some_success_response";
 	private static final String BLOCKED_BY_SERVER_RESPONSE = "some_blocked_by_server_response";
-	private static final String FAILURE_RESPONSE = "<tr>\\s*<th style=\"text-align:center;\">No crosses exist in the database for these items</th>\\s*</tr>";
+	private static final String FAILURE_RESPONSE = "No crosses exist in the database for these items";
 	private static final String INPUT_FIELD_ID = "fieldCrossReference1";
 	private static final String SEARCH_BUTTON_ID = "btnFindCrosses";
 	private static final ServerConnectionHandler connectionHandler = new JSBasedConnectionHandler(SERVER_URL_STRING, INPUT_FIELD_ID, SEARCH_BUTTON_ID);
@@ -71,5 +72,17 @@ public class BaldwinChecker extends FilterChecker {
 	@Override
 	public String getCheckerName() {
 		return CHECKER_NAME;
+	}
+	
+	@Override
+	public FilterEquivalents getEquivalentsFor(Filter filter) {
+		FilterEquivalents equivalents = super.getEquivalentsFor(filter);
+		
+		if(filter.getOemNumber().startsWith("0")){
+			Filter filterWithoutLeadingZeros = getFilterEquivalentForSuppliedFilterUsingOemNumberWithoutLeadingZeros(filter);
+			equivalents.addEquivalent(filterWithoutLeadingZeros);
+		}
+		
+		return equivalents;
 	}
 }
