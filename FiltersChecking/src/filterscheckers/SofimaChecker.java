dd10b30894226388054c4c3f28d6fc28122c9b9a
EroX <mamwasnahita@gmail.com>
2018-03-02 22:15:26 +0100

    Implemented base for SofimaChecker

---
+++ FiltersChecking/src/filterscheckers/SofimaChecker.java
@@ -0,0 +1,56 @@
+package filterscheckers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import connectionhandlers.JSBasedConnectionHandler;
+import connectionhandlers.ServerConnectionHandler;
+import models.FilterEquivalents;
+
+// TODO Problems with input field id - no input field id data in page's source code
+public class SofimaChecker extends FilterChecker {
+	private static final String CHECKER_NAME = "Sofima";
+	private static final String SERVER_URL_STRING = "http://catalogue.sofima.it/menu/menu.php?lingua=en";
+	private static final String SUCCESS_RESPONSE = "some_success_response";
+	private static final String BLOCKED_BY_SERVER_RESPONSE = "some_blocked_by_server_response";
+	private static final String INPUT_FIELD_ID = "SC_crossreferences_codicefiltrocostruttore2";
+	private static final String SEARCH_BUTTON_ID = "sc_b_pesq_bot";
+	private static final ServerConnectionHandler serverConnectionHandler = new JSBasedConnectionHandler(SERVER_URL_STRING, INPUT_FIELD_ID, SEARCH_BUTTON_ID);
+	
+	public SofimaChecker() {
+		super(CHECKER_NAME, serverConnectionHandler, SUCCESS_RESPONSE, BLOCKED_BY_SERVER_RESPONSE);
+	}
+
+	@Override
+	protected FilterEquivalents parseServerResponseAndGetEquivalents(String serverResponse) {
+		Pattern p = Pattern.compile(
+				  "");
+
+		Matcher m = p.matcher(serverResponse);
+		
+		String equivalentOEMNumber = null;
+		String equivalentOEM = null;
+		String equivalentNumber = null;
+		
+		FilterEquivalents equivalentsForThisOem = new FilterEquivalents();
+		
+		int propIdx = 1;
+		while(m.find()){
+			equivalentOEM = m.group(1);
+			equivalentOEMNumber = m.group(2);
+			equivalentNumber = m.group(3);
+
+			equivalentsForThisOem.createAndAddEquivalent(
+					getCheckerName(), 
+					equivalentOEMNumber, 
+					equivalentOEM, 
+					equivalentNumber, 
+					propIdx
+			);
+			
+			propIdx++;
+		}
+		
+		return equivalentsForThisOem;
+	}
+}
