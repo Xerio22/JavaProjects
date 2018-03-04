7b84fcf87f9f7cd37d4400d8b89c93498389d3ca
EroX <mamwasnahita@gmail.com>
2018-03-04 17:49:01 +0100

    Added exception handling for errorless application's flow and moved "Connected" message to the end of execJS method

--- FiltersChecking/src/connectionhandlers/JSBasedConnectionHandler.java
+++ FiltersChecking/src/connectionhandlers/JSBasedConnectionHandler.java
@@ -72,7 +72,7 @@ public class JSBasedConnectionHandler extends ServerConnectionHandler {
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
         webClient.getCookieManager().setCookiesEnabled(true);
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-        webClient.getOptions().setThrowExceptionOnScriptError(true); // TODO zmienic spowrotem na false do wersji produkcyjnej
+        webClient.getOptions().setThrowExceptionOnScriptError(false); // TODO zmienic spowrotem na false do wersji produkcyjnej
 	}
 	
 	
@@ -100,30 +100,50 @@ public class JSBasedConnectionHandler extends ServerConnectionHandler {
 	private String execJS(WebClient webClient) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		notifyObserverAboutChange(ServerConnectionHandler.CONNECTING_MESSAGE);
 		final HtmlPage pageToRequest = webClient.getPage(super.getServerUrlString());
-		notifyObserverAboutChange(ServerConnectionHandler.CONNECTED_MESSAGE);
         
         final HtmlTextInput filterNameInput = (HtmlTextInput) pageToRequest.getElementById(this.inputFieldId);
         DomElement findCrossesButton = pageToRequest.getElementById(this.searchButtonId);
         
-        if(notFoundById(findCrossesButton)) {
-        	findCrossesButton = getElementByClassName(pageToRequest); // as class	
+        if(notFoundInDom(findCrossesButton)) {
+        	findCrossesButton = getElementByClassName(pageToRequest); // find by class name	(first used in Mahle)
+        }
+        if(notFoundInDom(findCrossesButton)) {
+        	findCrossesButton = getElementByName(pageToRequest); // find by name (first user in Sakura)
         }
         
         filterNameInput.setValueAttribute(this.filterName);
-       
-        final HtmlPage page = findCrossesButton.click();
+         
         webClient.waitForBackgroundJavaScriptStartingBefore(0);
+        final HtmlPage page = findCrossesButton.click();
+//        webClient.waitForBackgroundJavaScriptStartingBefore(0);
+        
+        notifyObserverAboutChange(ServerConnectionHandler.CONNECTED_MESSAGE);
         
         return page != null ? page.asXml() : "";
 	}
-	
+
+	private boolean notFoundInDom(DomElement findCrosses) {
+		return findCrosses == null;
+	}
 	
 	private DomElement getElementByClassName(HtmlPage pageToRequest) {
-		return (DomElement) pageToRequest.querySelectorAll("." + this.searchButtonId).get(0);
+		DomElement elem = null;
+		try {
+			elem = (DomElement) pageToRequest.querySelectorAll("." + this.searchButtonId).get(0); // get might throw exception when didn't find by class
+		}
+		catch(Exception e){e.printStackTrace();}
+		
+		return elem;
 	}
 
-	private boolean notFoundById(DomElement findCrosses) {
-		return findCrosses == null;
+	private DomElement getElementByName(HtmlPage pageToRequest) {
+		DomElement elem = null;
+		try {
+			elem = (DomElement) pageToRequest.getElementByName(this.searchButtonId); // might throw exception when didn't find by name
+		}
+		catch(Exception e){}
+		
+		return elem;
 	}
 
 	private void notifyObserverAboutChange(String reconnectMessage) {
