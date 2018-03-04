ad393ca27be5f6307ab3b9d0e9494ef1c1af8e23
EroX <mamwasnahita@gmail.com>
2018-03-04 18:36:47 +0100

    Added base for FiltrationGroupChecker

---
+++ FiltersChecking/src/filterscheckers/JSBased/FiltrationGroupChecker.java
@@ -0,0 +1,58 @@
+package filterscheckers.JSBased;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import connectionhandlers.JSBasedConnectionHandler;
+import connectionhandlers.ServerConnectionHandler;
+import filterscheckers.FilterChecker;
+import models.FilterEquivalents;
+
+// TODO input by name + search button by ..?
+public class FiltrationGroupChecker extends FilterChecker {
+	private static final String CHECKER_NAME = "FiltrationGroup";
+	private static final String SERVER_URL_STRING = "https://www.filtrationgroup.com/content/mahle-reference";
+	private static final String SUCCESS_RESPONSE = "";
+	private static final String BLOCKED_BY_SERVER_RESPONSE = "some_blocked_by_server_response";
+	private static final String INPUT_FIELD_NAME = "";
+	private static final String SEARCH_BUTTON_NAME = "";
+	private static final ServerConnectionHandler SERVER_CONNECTION_HANDLER = new JSBasedConnectionHandler(SERVER_URL_STRING, INPUT_FIELD_NAME, SEARCH_BUTTON_NAME);
+	
+	public FiltrationGroupChecker() {
+		super(CHECKER_NAME, SERVER_CONNECTION_HANDLER, SUCCESS_RESPONSE, BLOCKED_BY_SERVER_RESPONSE);
+	}
+	
+	@Override
+	protected FilterEquivalents parseServerResponseAndGetEquivalents(String serverResponse) {
+		Pattern p = Pattern.compile(
+				  ""
+				);
+
+		Matcher m = p.matcher(serverResponse);
+		
+		String equivalentOEMNumber = null;
+		String equivalentOEM = null;
+		String equivalentNumber = null;
+		
+		FilterEquivalents equivalentsForThisOem = new FilterEquivalents();
+		
+		int propIdx = 1;
+		while(m.find()){
+			equivalentOEM = m.group(1);
+			equivalentOEMNumber = m.group(2);
+			equivalentNumber = m.group(3);
+
+			equivalentsForThisOem.createAndAddEquivalent(
+					getCheckerName(), 
+					equivalentOEMNumber, 
+					equivalentOEM, 
+					equivalentNumber, 
+					propIdx
+			);
+			
+			propIdx++;
+		}
+		
+		return equivalentsForThisOem;
+	}
+}
