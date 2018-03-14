1bd798a1656bdb8b08558d8067b98833c629acb5
EroX <mamwasnahita@gmail.com>
2018-03-14 18:48:45 +0100

    Lowered reconnect time and reconnect tries

--- FiltersChecking/src/connectionhandlers/ServerConnectionHandler.java
+++ FiltersChecking/src/connectionhandlers/ServerConnectionHandler.java
@@ -7,8 +7,8 @@ public abstract class ServerConnectionHandler extends Observable {
 	public static final String CONNECTING_MESSAGE = "Connecting";
 	public static final String CONNECTED_MESSAGE = "Connected";
 	public static final String URL_ERROR = "URL_error";
-	public static int RECONNECT_TIME = 10000;
-	public static int RECONNECT_TRIES = 5;
+	public static int RECONNECT_TIME = 5000;
+	public static int RECONNECT_TRIES = 3;
 	private String serverUrlString;
 	
 	public ServerConnectionHandler(String serverUrlString) {
